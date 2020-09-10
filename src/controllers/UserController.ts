aec4ca3d9fed2bae342d29969e010ccd84d9586f
pandazim <jaotoledo74@gmail.com>
2020-09-09 20:31:45 -0400

    metodo GET que recebe ID então mostra todos os usuarios da requisiçao

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,33 @@
 import { Request, Response, response } from 'express'
 import db from '../database/connections'
+import Knex from 'knex'
 
 
 export default class UserController {
-    async index(req: Response, res: Response) {
-        const users = await db('users')
+    async index(req: Request, res: Response) {
 
-        return res.json(users)
+        const user = await db('users')
+
+        const { id } = req.params
+
+        try {
+            await db('users')
+                .where('id', id)
+                .then((value) => {
+                    if(value[0] == null) {
+                        res.status(400).json({
+                            message: "No users in the database yet"
+                        })
+                    } else {
+                        res.status(200).json(user)
+                    }
+                })
+        } catch(err) {
+            console.log(err)
+            res.status(400).json({
+                message: 'something happened'
+            })
+        }
     }
 
     async create(req: Request, res: Response) {
@@ -19,6 +40,7 @@ export default class UserController {
             cpf
         } = req.body
 
+        
         
         try {
 
@@ -56,6 +78,71 @@ export default class UserController {
                 error: "falta coisa"
             })
 
+        }
+    }
+
+    async update(req: Request, res: Response) {
+
+        console.log(req.body)
+        const {
+            id,
+            nome,
+            sobreNome,
+            dataNascimento,
+        } = req.body
+
+
+        try {
+
+            await db('users')
+            .where('id', id)
+            .select('id')
+            .then(async (value)=> {
+                
+                if(value[0] == null){
+                    res.status(400).json({
+                        error: "usuario não encontrado"
+                    })
+                } else {
+                    await db('users').where('id', id)
+                    .update({
+                        nome,
+                        sobreNome,
+                        dataNascimento,
+                    })
+                    res.status(200).json({
+                        message: "Atualizado com sucesso"
+                    })
+                }
+            })
+
+        } catch(err){
+            return res.status(400).json({
+                error: "ID não encontrado"
+            })
+        } 
+    }
+
+    async delete(req: Request, res: Response) {
+        
+        console.log(req.params)
+
+        const { id } = req.params
+        
+
+        try {
+            await db('users')
+            .where('id', id)
+            .del()
+            .then(() => {
+                res.json({ message: "SUCESSO"})
+            })
+
+        } catch (err) {
+            console.log(err)
+            return res.status(400).json({
+                error: "ID não encontrado"
+            })
         }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ const { default: UserController } = require('./controllers/UserController');
 const userController = new UserController
 const routes = express.Router();
 
-routes.get('/users', userController.index)
+routes.get('/users/:id', userController.index)
 routes.post('/users', userController.create)
+routes.put('/users', userController.update)
+routes.delete('/users/:id', userController.delete)
 
 module.exports = routes
